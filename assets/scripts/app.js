91348a3ae25686d8964cd42259fbf8d985b4ccf6
Parhamkhd <31856253+Parhamkhd@users.noreply.github.com>
2019-03-25 12:27:26 +0430

    added hamburger icon & some media queries

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -42,3 +42,10 @@ var x = setInterval(function() {
 
   }
 }, 1000);
+
+
+$(document).ready(function(){
+	$('#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4').click(function(){
+		$(this).toggleClass('open');
+	});
+});
