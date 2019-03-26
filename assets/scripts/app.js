38a306dc2b5be4f762c6542afa4aaa2609a72fbd
Parhamkhd <31856253+Parhamkhd@users.noreply.github.com>
2019-03-26 15:38:07 +0430

    added mobile menu items

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,51 +1,47 @@
-
-// Set the date we're counting down to
-var countDownDate = new Date("Apr 23, 2019 15:37:25").getTime();
-
-// Update the count down every 1 second
-var x = setInterval(function() {
-
-  // Get todays date and time
-  var now = new Date().getTime();
     
-  // Find the distance between now and the count down date
-  var distance = countDownDate - now;
     
-  // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    document.getElementById('login').addEventListener('click' ,function(){
+        document.getElementById('overlay').style.display = 'block';
+        document.getElementById('login-overlay').style.display = 'block';
+        document.getElementById('overlay').classList.add('animated', 'fadeInDown');
+        document.getElementById('login-overlay').classList.add('animated', 'fadeInDown');
+
+
+
+
+    });
+
+    document.getElementById('overlay').addEventListener('click', function(){
+        document.getElementById('overlay').style.display ='none';
+        document.getElementById('login-overlay').style.display = 'none';
+
+    });
+
+    document.getElementById('cancel').addEventListener('click', function(){
+        document.getElementById('overlay').style.display ='none';
+        document.getElementById('login-overlay').style.display = 'none';
+
+    });
+
+    var mobileDisplay = document.getElementById('mobileMenu');
+
+function openClose(){
+    if(mobileDisplay.style.display == ''){
+        mobileDisplay.style.display = "block";
+        document.getElementById('mobileMenu').classList.add('animated', 'fadeInDown');
+
+    }else if(mobileDisplay.style.display == 'block'){
+        mobileDisplay.style.display = "";
+
+    }
     
-  // Output the result in an element with id="demo"
-  document.getElementById("days-num").innerText = days;
-
-  document.getElementById("hours-num").innerHTML = hours;
-
-  document.getElementById("minutes-num").innerHTML = minutes;
-
-  document.getElementById("seconds-num").innerHTML = seconds;
-
-
-
+}
     
- // If the count down is over, write some text 
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("days-num").innerHTML = 'Expired';
 
-    document.getElementById("hours-num").innerHTML = "Expired";
-  
-    document.getElementById("minutes-num").innerHTML = "Expired";
+$(window).resize(function() {
+    if(window.innerWidth >= 990){
+        mobileDisplay.style.display = "";
+        document.getElementById('nav-icon3').classList.remove('open');
 
-    document.getElementById("seconds-num").innerHTML = "Expired";
-
-  }
-}, 1000);
-
-
-$(document).ready(function(){
-	$('#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4').click(function(){
-		$(this).toggleClass('open');
-	});
+    }
 });
